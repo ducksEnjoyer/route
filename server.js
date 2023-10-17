bd03dd405b1b8a8abfdd681833d1d1c9603b8fd9
ducksEnjoyer <atomenjoyer325@gmail.com>
2023-10-17 17:57:43 +0200

    nothing wacky going on here

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require("express")
 const app = express() 
 const mainRouter = require(__dirname + "/routes/main.js") //
 const secondRouter = require(__dirname + "/routes/second.js")
-
+const PORT = process.env.PORT || 8000;
 app.use("/",mainRouter)
 app.use("/",secondRouter)
 app.use(express.static('static'));
-app.listen(8000,console.log("working"))
+app.listen(PORT,console.log("working"))
